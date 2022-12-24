dc1487e577f119fce26c12dad771806d22ecf19b
ArlindaHaxha <84712856+ArlindaHaxha@users.noreply.github.com>
2022-12-24 11:59:29 -0800

    Add files via upload

---
+++ HZHWProject/Controllers/ProfessorController.cs
@@ -0,0 +1,148 @@
+using HZHWProject.Configuration;
+using HZHWProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication20.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly UPD8DbContextProfessor _context;
+
+        public UsersController(UPD8DbContextProfessor context)
+        {
+            _context = context;
+        }
+
+        // GET: Users
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Professor.ToListAsync());
+        }
+
+        // GET: Users/Details/5
+        public async Task<IActionResult> Details(int? ID)
+        {
+            if (ID == null)
+            {
+                return NotFound();
+            }
+
+            var professor = await _context.Professor
+                .FirstOrDefaultAsync(m => m.ID == ID);
+            if (professor == null)
+            {
+                return NotFound();
+            }
+
+            return View(professor);
+        }
+
+        // GET: Users/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Users/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,username,password,email")] Professor professor)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(professor);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(professor);
+        }
+
+        // GET: Users/Edit/5
+        public async Task<IActionResult> Edit(int? ID)
+        {
+            if (ID == null)
+            {
+                return NotFound();
+            }
+
+            var professor = await _context.Professor.FindAsync(ID);
+            if (professor == null)
+            {
+                return NotFound();
+            }
+            return View(professor);
+        }
+
+        // POST: Users/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int ID, [Bind("ID,username,password,email")] Professor professor)
+        {
+            if (ID != professor.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(professor);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UserExists(professor.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(professor);
+        }
+
+        // GET: Users/Delete/5
+        public async Task<IActionResult> Delete(int? ID)
+        {
+            if (ID == null)
+            {
+                return NotFound();
+            }
+
+            var professor = await _context.Professor
+                .FirstOrDefaultAsync(m => m.ID == ID);
+            if (professor == null)
+            {
+                return NotFound();
+            }
+
+            return View(professor);
+        }
+
+        // POST: Users/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int ID)
+        {
+            var professor = await _context.Professor.FindAsync(ID);
+            _context.Professor.Remove(professor);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UserExists(int ID)
+        {
+            return _context.Professor.Any(e => e.ID == ID);
+        }
+    }
+}
