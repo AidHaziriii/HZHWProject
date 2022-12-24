dd0cb090e2a4cebae37a67578b88ccb3c9fb0864
Leorenti <ls52728@ubt-uni.net>
2022-12-24 19:08:31 +0100

    Leo'Cruds

---
+++ HZHWProject/Models/Admin.cs
@@ -0,0 +1,11 @@
+﻿namespace HZHWProject.Models
+{
+    public class Admin
+    {
+        public int ID { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public string email { get; set; }
+
+    }
+}
